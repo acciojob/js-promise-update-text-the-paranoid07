c08e987fa9e003d3afc636278e9f17636caaae20
the-paranoid07 <121954633+the-paranoid07@users.noreply.github.com>
2023-06-29 19:36:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
-//your JS code here. If required.
-
 function callme() {
-	return new Promise((resolve)=>{
-		setTimeOut(()=>{
-			resolve("Hello, world!");
-		},1000)
-	})
+	return new Promise(resolve => {
+        setTimeout(()=>{
+
+            resolve("Hello, world!");
+        },1000)
+    })
 }
 
 callme().then( (data) => {
 	const htmlDoc=document.getElementById("output");
 	htmlDoc.innerText=data;
 })
+
+callme();
